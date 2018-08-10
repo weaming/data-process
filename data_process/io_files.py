34e6723caaa9f091f25956d779486f2babed179b
weaming <garden.yuen@gmail.com>
2018-08-10 15:32:49 +0800

    remove print

--- data_process/io_files.py
+++ data_process/io_files.py
@@ -6,7 +6,6 @@ def file_list_in(root, pattern, **kwargs):
     if root[-1] != '/':
         root += '/'
     pattern = root + pattern
-    print(pattern)
     rv = glob.glob(pattern, **kwargs)
     rv = [x[len(root):] for x in rv]
     return rv
