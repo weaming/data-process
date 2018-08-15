6caa7ec54a8151d67bca6c271f5402c68b058a28
weaming <garden.yuen@gmail.com>
2018-08-15 15:10:56 +0800

    add join prefix

--- data_process/join.py
+++ data_process/join.py
@@ -3,7 +3,17 @@ from itertools import chain
 from .group_by import group_by
 
 
-def _join(data_list_a, data_list_b, a_get_key, b_get_key, left=True, right=True):
+def get_count_in_list(lst, k):
+    return sum(map(lambda x: 1 if x == k else 0, lst))
+
+
+def get_unique_key(lst, key, prefix):
+    if get_count_in_list(lst, key) > 1:
+        return prefix + key
+    return key
+
+
+def _join(data_list_a, data_list_b, a_get_key, b_get_key, left=True, right=True, a_prefix='a.', b_prefix='b.'):
     """
     condition: data's key is unique
 
@@ -16,12 +26,14 @@ def _join(data_list_a, data_list_b, a_get_key, b_get_key, left=True, right=True)
     :return:
     """
 
+    all_origin_fields = list(chain(data_list_a[0].keys(), data_list_b[0].keys()))
     a_groups = group_by(data_list_a, a_get_key)
     b_groups = group_by(data_list_b, b_get_key)
 
     # extract the only one data
-    a_groups = {k: a_groups[k][0] for k in a_groups.keys()}
-    b_groups = {k: b_groups[k][0] for k in b_groups.keys()}
+    a_groups = {get_unique_key(all_origin_fields, k, a_prefix): a_groups[k][0] for k in a_groups.keys()}
+
+    b_groups = {get_unique_key(all_origin_fields, k, b_prefix): b_groups[k][0] for k in b_groups.keys()}
 
     all_fields = chain(a_groups.keys(), b_groups.keys())
 
@@ -39,17 +51,17 @@ def _join(data_list_a, data_list_b, a_get_key, b_get_key, left=True, right=True)
     return rv
 
 
-def left_join(a, b, a_get_key, b_get_key):
-    return _join(a, b, a_get_key, b_get_key, left=True, right=False)
+def left_join(a, b, a_get_key, b_get_key, **kwargs):
+    return _join(a, b, a_get_key, b_get_key, left=True, right=False, **kwargs)
 
 
-def right_join(a, b, a_get_key, b_get_key):
-    return _join(a, b, a_get_key, b_get_key, left=False, right=True)
+def right_join(a, b, a_get_key, b_get_key, **kwargs):
+    return _join(a, b, a_get_key, b_get_key, left=False, right=True, **kwargs)
 
 
-def inner_join(a, b, a_get_key, b_get_key):
-    return _join(a, b, a_get_key, b_get_key, left=False, right=False)
+def inner_join(a, b, a_get_key, b_get_key, **kwargs):
+    return _join(a, b, a_get_key, b_get_key, left=False, right=False, **kwargs)
 
 
-def outer_join(a, b, a_get_key, b_get_key):
-    return _join(a, b, a_get_key, b_get_key, left=True, right=True)
+def outer_join(a, b, a_get_key, b_get_key, **kwargs):
+    return _join(a, b, a_get_key, b_get_key, left=True, right=True, **kwargs)
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 name = 'data-process'
 gh_repo = 'https://github.com/weaming/{}'.format(name)
-version = '0.1.9'
+version = '0.1.10'
 
 setup(
     name=name,  # Required
