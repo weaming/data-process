9c95ce81b8138ed56f27f1dbf06ef0de153200c5
weaming <garden.yuen@gmail.com>
2018-08-17 15:33:00 +0800

    add tail_list_fn

--- data_process/group_by.py
+++ data_process/group_by.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from .dict_helper import infinite_default_dict
 
 
-def group_by(data_list, get_group_key, item_fn=None):
+def group_by(data_list, get_group_key, item_fn=None, tail_list_fn=None):
     groups = defaultdict(list)
     for x in data_list:
         key = get_group_key(x)
@@ -11,15 +11,31 @@ def group_by(data_list, get_group_key, item_fn=None):
                 groups[k].append(item_fn(x) if item_fn else x)
         else:
             groups[key].append(item_fn(x) if item_fn else x)
+
+    if tail_list_fn:
+        return {k: tail_list_fn(v) for k, v in groups.items()}
+
     return dict(groups)
 
 
-def group_by_function_list(data_list, get_key_fn_list, item_fn=None):
+def group_by_function_list(
+    data_list, get_key_fn_list, item_fn=None, tail_list_fn=None
+):
     if len(get_key_fn_list) == 1:
-        return group_by(data_list, get_key_fn_list[0], item_fn=item_fn)
+        return group_by(
+            data_list,
+            get_key_fn_list[0],
+            item_fn=item_fn,
+            tail_list_fn=tail_list_fn,
+        )
     else:
         groups = group_by(data_list, get_key_fn_list[0])
         return {
-            k: group_by_function_list(v, get_key_fn_list[1:], item_fn=item_fn)
+            k: group_by_function_list(
+                v,
+                get_key_fn_list[1:],
+                item_fn=item_fn,
+                tail_list_fn=tail_list_fn,
+            )
             for k, v in groups.items()
         }
