d3b72323a91b6c08fe67aa15a37ae398a53678ff
weaming <garden.yuen@gmail.com>
2018-08-17 13:44:38 +0800

    add io_json

---
+++ data_process/io_json.py
@@ -0,0 +1,16 @@
+import os
+import json
+from .green_dict import json_dumps
+
+
+def read_json(fp):
+    if not os.path.exists(fp):
+        return None
+
+    with open(fp) as f:
+        return json.loads(f.read())
+
+
+def save_json(fp, data):
+    with open(fp, "w") as f:
+        return f.write(json_dumps(data))
