c49cee5f4fb10b90c2984d8e321507d393b2b95b
weaming <garden.yuen@gmail.com>
2018-08-17 15:12:37 +0800

    add item_fn

--- data_process/group_by.py
+++ data_process/group_by.py
@@ -2,24 +2,24 @@ from collections import defaultdict
 from .dict_helper import infinite_default_dict
 
 
-def group_by(data_list, get_group_key):
+def group_by(data_list, get_group_key, item_fn=None):
     groups = defaultdict(list)
     for x in data_list:
         key = get_group_key(x)
         if isinstance(key, (list, tuple)):
             for k in key:
-                groups[k].append(x)
+                groups[k].append(item_fn(x) if item_fn else x)
         else:
-            groups[key].append(x)
+            groups[key].append(item_fn(x) if item_fn else x)
     return groups
 
 
-def group_by_function_list(data_list, get_key_fn_list):
+def group_by_function_list(data_list, get_key_fn_list, item_fn=None):
     if len(get_key_fn_list) == 1:
-        return group_by(data_list, get_key_fn_list[0])
+        return group_by(data_list, get_key_fn_list[0], item_fn=item_fn)
     else:
         groups = group_by(data_list, get_key_fn_list[0])
         return {
-            k: group_by_function_list(v, get_key_fn_list[1:])
+            k: group_by_function_list(v, get_key_fn_list[1:], item_fn=item_fn)
             for k, v in groups.items()
         }
