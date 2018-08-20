2cbc8989d4cc29494f38e37a8e146562a7f6c637
weaming <garden.yuen@gmail.com>
2018-08-20 14:42:02 +0800

    ensure unicode when io csv

--- data_process/io_csv.py
+++ data_process/io_csv.py
@@ -16,11 +16,9 @@ import sys
 import os
 import io
 from contextlib import contextmanager
+import fuck_str
 
-CSV_FORMAT_PARAMS = dict(
-    delimiter=',',
-    quotechar='"',
-)
+CSV_FORMAT_PARAMS = dict(delimiter=",", quotechar='"')
 
 
 @contextmanager
@@ -29,11 +27,17 @@ def new_csv_writer(path, fields, csv_format=None, keep_open=False):
     try:
         if path is None:
             f = sys.stdout
-        elif hasattr(path, 'write'):
+        elif hasattr(path, "write"):
             f = path
         else:
-            f = io.open(path, 'w', encoding='utf-8', newline='')
-        yv = csv.DictWriter(f, fieldnames=fields, **(csv_format or CSV_FORMAT_PARAMS))
+            f = io.open(path, "w", encoding="utf-8", newline="")
+
+        # ensure unicode
+        fields = [fuck_str.ensure_unicode(x) for x in fields]
+
+        yv = csv.DictWriter(
+            f, fieldnames=fields, **(csv_format or CSV_FORMAT_PARAMS)
+        )
         yv.writeheader()
         yield yv
     finally:
@@ -45,20 +49,28 @@ def new_csv_writer(path, fields, csv_format=None, keep_open=False):
 def new_csv_reader(path, fields=None, csv_format=None):
     f = None
     try:
-        if hasattr(path, 'read'):
+        if hasattr(path, "read"):
             f = path
         else:
-            f = io.open(path, 'r', encoding='utf-8')
-        yield csv.DictReader(f, fieldnames=fields, **(csv_format or CSV_FORMAT_PARAMS))
+            f = io.open(path, "r", encoding="utf-8")
+        yield csv.DictReader(
+            f, fieldnames=fields, **(csv_format or CSV_FORMAT_PARAMS)
+        )
     finally:
         f and f.close()
 
 
 def _process_row(writer, row):
+    row = {
+        fuck_str.ensure_unicode(k): fuck_str.ensure_unicode(v)
+        for k, v in row.items()
+    }
     writer.writerow(row)
 
 
-def process_row_generator(fields, generator, output, process_row=_process_row, **kwargs):
+def process_row_generator(
+    fields, generator, output, process_row=_process_row, **kwargs
+):
     c = 0
 
     with new_csv_writer(output, fields, **kwargs) as writer:
@@ -82,10 +94,9 @@ def merge_csv_list(file_path_list, fields=None, csv_format=None):
 
     for fp in file_path_list:
         if not os.path.exists(fp):
-            raise Exception('file {} does not exist'.format(fp))
+            raise Exception("file {} does not exist".format(fp))
         with new_csv_reader(fp, fields=fields, csv_format=csv_format) as reader:
             for row in reader:
                 _process_row(row)
 
     return rv
-
--- test_io_csv.py
+++ test_io_csv.py
@@ -2,9 +2,12 @@ from data_process.io_csv import new_csv_reader, process_row_generator
 
 path = 'test.csv'
 outpath = 'test.out.csv'
+
+print('test read')
 with new_csv_reader(path) as csv:
     data = list(csv)
 
 print(data)
 
+print('test write')
 process_row_generator(list(data[0]), iter(data), outpath)
